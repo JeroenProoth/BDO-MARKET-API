333f52f9bbfff1f5ce28e7419ec6a4c7eabdf71a
Jeroen Prooth <jeroen.prooth@gmail.com>
2021-01-02 10:46:54 +0100

    Update

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # BDO-MARKET-API
  Python Library for Black Desert Online marketplace.
+
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#resources">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+    </li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+Thanks to [Ruwann](https://github.com/Ruwann) for helping me figure out how to read the Google console.
+
+I tried to build a Python library to communicate with the Black Desert Online marketplace.
+
+### Resources
+* [Curlconverter](https://github.com/NickCarneiro/curlconverter) by NickCarneiro
+* [bdo-marketplace](https://github.com/kookehs/bdo-marketplace)  by kookehs
+
+
+## Getting Started
+
+
+## Contact
+
+[![Issues][issues-shield]][issues-url]
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/JeroenProoth/BDO-MARKET-API.svg?style=for-the-badge
+[contributors-url]: https://github.com/JeroenProoth/BDO-MARKET-API/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/JeroenProoth/BDO-MARKET-API.svg?style=for-the-badge
+[forks-url]: https://github.com/JeroenProoth/BDO-MARKET-API/network/members
+[stars-shield]: https://img.shields.io/github/stars/JeroenProoth/BDO-MARKET-API.svg?style=for-the-badge
+[stars-url]: https://github.com/JeroenProoth/BDO-MARKET-API/stargazers
+[issues-shield]: https://img.shields.io/github/issues/JeroenProoth/BDO-MARKET-API.svg?style=for-the-badge
+[issues-url]: https://github.com/JeroenProoth/BDO-MARKET-API/issues
+
--- market_api/calls.py
+++ market_api/calls.py
@@ -3,15 +3,65 @@ from .client import Client
 import pandas as pd
 
 class Calls(Client):
+    item_list = pd.read_csv('market_api\\itemsids.csv', sep = ",")
 
     def __init__(self, session_id, cookie_token, form_token):
         super().__init__(session_id, cookie_token, form_token)
 
-    def get_market_depth(self, item_id):
-        self.set_item(item_id)
-        data = self.connect(method = '/Home/GetItemSellBuyInfo')['marketConditionList']
 
+    def get_item_id(self, item_name):
+        try:
+            return self.item_list.loc[self.item_list.Item == str(item_name)].ID.values[0]
+        except IndexError:
+            raise IndexError("Item with name '{}' not found.".format(item_name))
+
+    def get_item_name(self, item_id):
+        try:
+            return self.item_list.loc[self.item_list.ID == int(item_id)].Item.values[0]
+        except IndexError:
+            return None
+
+    def get_item_sell_buy_info(self, item_id):
+
+        self.set_item(item_id)
+        data = self.connect(method = '/Home/GetItemSellBuyInfo')
+
+        return data
+
+    def get_market_depth(self, item_id, dump = False):
+        """Returns a pandas DataFrame sorted from low -> high.
+        Market depth is given by the buy- and sellCounts. 
+        """
+        
+        data = self.get_item_sell_buy_info(item_id)['marketConditionList']
         market_conditions = pd.DataFrame.from_dict(data)
+
+        if dump:
+            market_conditions.to_csv()
 
         # Return a DataFrame sorted by given column order.
         return market_conditions[['pricePerOne', 'buyCount', 'sellCount']]
+
+    def get_latest_price(self, item_id, trigger = 'None'):
+        """Returns the latest price based on buyCount or sellCount (default).
+        If 'buyCount' is used, it returns the highest price someone is willing to pay for it.
+        If 'sellCount' is used, it returns the lowest price someone is willing to sell it for.
+
+        If there are no sellorders (as in sellCount == 0 at every pricepoint), return max price.
+        """
+
+        market_conditions = self.get_market_depth(item_id)
+
+        if trigger == 'buyCount':
+            market_price = market_conditions.loc[market_conditions['buyCount'] != 0].max()['pricePerOne']
+            if not pd.isna(market_price):
+                return market_price
+            else:
+                return market_conditions['pricePerOne'].max()
+        else:
+            market_price = market_conditions.loc[market_conditions['sellCount'] != 0].min()['pricePerOne']
+            if not pd.isna(market_price):
+                return market_price
+            else:
+                return market_conditions['pricePerOne'].max()
+
