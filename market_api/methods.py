ca939129bfe5993f28b5c46862c3d4ee7f31b851
JeroenProoth <33283073+JeroenProoth@users.noreply.github.com>
2021-01-03 10:48:23 +0100

    Added docstrings

--- market_api/methods.py
+++ market_api/methods.py
@@ -12,12 +12,67 @@ class Methods(Client):
         super().__init__(session_id, cookie_token, form_token)
 
     def get_item_sell_buy_info(self, item_id):
+        """Returns market information of a given item_id
+
+        {
+            "priceList" : [ list of prices item can be sold at],
+            "marketConditionList" : [
+                {
+                "sellCount"     : Amount of items being sold at this pricepoint,
+                "buyCounts"     : Amount of items being bought at this pricepoint,
+                "pricePerOne"   : Pricepoint
+                },
+                ...
+            ],
+            "basePrice"                     : Minimum the item can be sold for (not minimum it is listed for),
+            "enchantGroup"                  : No idea,
+            "enchantMaterialKey"            : No idea,
+            "enchantMaterialPrice"          : No idea,
+            "enchantNeedCount"              : No idea,
+            "maxRegisterForWorldMarket"     : Maximum amount that can be registered at once,
+            "countValue"                    : No idea,
+            "sellMaxCount"                  : No idea,
+            "buyMaxCount"                   : No idea,
+            "resultCode"                    : Resulting code,
+            "resultMsg" : '[
+                {
+                "days"  :  Date,
+                "Value" : Value of item at that data
+                },
+                ...
+            ]'
+        }
+
+        """
         self.set_item(item_id)
         data = self.connect(method = self.GETITEMSELLBUYINFO)
 
         return data
 
     def get_world_market_sub_list(self, item_id):
+        """Returns detailList of a given item_id
+        
+        {
+            "detailList" : [
+                {
+                "pricePerOne"       : Price of one item,
+                "totalTradeCount"   : Total amount of items traded up till now,
+                "keyType"           : No idea,
+                "mainKey"           : ID of the item,
+                "subKey"            : No idea,
+                "count"             : Stock of the item,
+                "name"              : Name of the item,
+                "grade"             : Grade of the item,
+                "mainCategory"      : Main category item belongs to,
+                "subCategory"       : Sub category item belongs to,
+                "chooseKey"         : No idea,                
+                }
+            ],
+            "resultCode"     : Resulting code,
+            "resultMsg"      : Resulting message
+        }
+
+        """
         self.set_item(item_id)
         data = self.connect(method = self.GETWORLDMARKETSUBLIST)
 
@@ -27,7 +82,7 @@ class Methods(Client):
         """Returns a list of items based on main_category and sub_category
 
         {
-            "marketList": [
+            "marketList" : [
                 {
                 "mainKey"   : ID of the item,
                 "sumCount"  : Amount currently posted on the market, 
